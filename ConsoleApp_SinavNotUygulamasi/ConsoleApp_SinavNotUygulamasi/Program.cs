9d324f4b0a16843944a6c5365a1cb058c2d3f0e3
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2023-09-20 00:27:03 +0300

    added

---
+++ ConsoleApp_SinavNotUygulamasi/ConsoleApp_SinavNotUygulamasi/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool ogrenciSinavgirdimi = true;
+        int sinavNot = 86;
+        
+
+
+        if (ogrenciSinavgirdimi == true)
+        {
+
+            if ( sinavNot <= 100 && sinavNot >= 90)
+            {
+                Console.WriteLine("Sınav harf notunuz: " + sinavNot + "- AA");
+            }
+            else if (sinavNot >= 80)
+            {
+                Console.WriteLine("Sınav harf notunuz: " + sinavNot + "- BA");
+            }
+            else if (sinavNot >= 70)
+            {
+                Console.WriteLine("Sınav harf notunuz: " + sinavNot + "- CB");
+            }
+            else if (sinavNot >= 50)
+            {
+                Console.WriteLine("Sınav harf notunuz: " + sinavNot + "- DD");
+            }
+            else
+            {
+                Console.WriteLine("Sınav harf notunuz: " + sinavNot + "- FF");
+            }
+
+        }
+        else
+        {
+            Console.WriteLine(" Sınava giriş yapmadınız ");
+        }
+    }
+}
